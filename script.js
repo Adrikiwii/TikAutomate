3e917e83eab0d1e6aa4aa8d2139f24130f6214a5
adrien <73719688+Adrikiwii@users.noreply.github.com>
2025-03-14 16:30:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -59,8 +59,9 @@ function generateTikTokAuthUrl(clientId, redirectUri, scope, state = null) {
     }).then((response) => {
       return response.json()
     }).then((result) => {
-      console.log(result.creator_avatar_url);
-      console.log(result.display_name);
+        console.log(result);
+      //console.log(result.creator_avatar_url);
+      //console.log(result.display_name);
     })
   }
 
